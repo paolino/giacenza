a01cd039647abfae4e6572f4c049dfc396992533
paolo <paolo.veronelli@gmail.com>
2023-07-22 07:12:49 +0100

    add number parsing tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Lib (NumberFormat (NumberFormat), parseValue)
+import Test.Hspec (describe, hspec, it, shouldBe)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "parsing values" $ do
+        it "parses a positive value" $ do
+            parseValue (NumberFormat ',' '.') "1.234,56"
+                `shouldBe` Right 1234.56
+        it "parses a negative value" $ do
+            parseValue (NumberFormat ',' '.') "-1.234,56"
+                `shouldBe` Right (-1234.56)
+        it "parses a value with no decimals" $ do
+            parseValue (NumberFormat ',' '.') "1.234"
+                `shouldBe` Right 1234
+        it "parses a value with no decimals and no thousands separator" $ do
+            parseValue (NumberFormat ',' '.') "1234"
+                `shouldBe` Right 1234
+        it "parses a positive value in american format" $ do
+            parseValue (NumberFormat '.' ',') "1,234.56"
+                `shouldBe` Right 1234.56
+        it "parses a negative value in american format" $ do
+            parseValue (NumberFormat '.' ',') "-1,234.56"
+                `shouldBe` Right (-1234.56)
+    describe "parding dates"
